b1372023dc36a1752de751de86091272f66c2857
Diego Osuna <dieouna@gmail.com>
2025-12-10 21:34:10 -0500

    Added function and domainList & listElement

--- src/app.js
+++ src/app.js
@@ -6,22 +6,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function () {
   //write your code here
+  const domainList = document.querySelector("#list-of-domains")
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
+  let domain = ["com", "info", "biz", "in", "net"];
 
-  const excuseLine = document.querySelector("#excuse");
+  
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let p = 0; p < noun.length; p++) {
-        for (let r = 0; r < dominio.length; r++) {
-          console.log(`${pronoun[i]}${adj[j]}${noun[p]}${dominio[r]}`);
+        for (let r = 0; r < domain.length; r++) {
+          console.log(`${pronoun[i]}${adj[j]}${noun[p]}.${domain[r]}`);
+          const listElement = document.createElement("li")
+          listElement.textContent = `${pronoun[i]}${adj[j]}${noun[p]}.${domain[r]}`
+          domainList.appendChild(listElement)
         }
+        
       }
     }
   }
 };
 
-
-
-console.log(`${pronoun[i]} ${adj[j]} ${noun[p]} $`);
